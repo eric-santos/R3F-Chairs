c36476ba02757e3524b4173855f6280b61f58a48
eric santos <eric355402@yahoo.com>
2021-03-27 13:55:18 -0600

    created canvas and created model to import gltf, created htmlContent and nested in a Suspense to solve load error

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./App.scss";
 //Components
 import Header from "./components/header";
+import { Section } from "./components/section";
+import { Canvas } from "react-three-fiber";
+
+import { Html, useGLTFLoader } from "drei";
+
+const Model = () => {
+  const gltf = useGLTFLoader("/armchairYellow.gltf", true);
+  return <primitive object={gltf.scene} dispose={null} />;
+};
+
+const HTMLContent = () => {
+  return (
+    <Section factor={1.5} offset={1}>
+      <group position={[0, 250, 0]}>
+        <mesh position={[0, 35, 0]}>
+          <Model />
+        </mesh>
+        <Html fullscreen>
+          <div className="container">
+            <h1 className="title">Hello</h1>
+          </div>
+        </Html>
+      </group>
+    </Section>
+  );
+};
 
 export default function App() {
   return (
     <>
       <Header />
-      <div className='container'>
-        <div className='title'>
-          <h1>Stater Branch</h1>
-        </div>
-      </div>
+      <Canvas colorManagement camera={{ position: [0, 0, 120], fov: 70 }}>
+        
+        <Suspense fallback={null}>
+        <HTMLContent />
+        </Suspense>
+      </Canvas>
     </>
   );
 }
